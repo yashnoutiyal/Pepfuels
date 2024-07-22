8ec5db5a4d884fb41f04eff58b64ad32de188026
Harpreet Singh <84502026+Harpreetsingh10414@users.noreply.github.com>
2024-07-22 21:11:59 +0530

    Added serverjs file with env setup and install node modules

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
